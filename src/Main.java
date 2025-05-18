771c2790abd96a8b3c6bfcf20e396578ec356eed
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-05-18 00:32:48 -0300

    feat: add user input and insert logic for username, passowrd, email, and CPF

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,31 @@ public class Main {
         String user = "root";
         String password = "1234";
 
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Scanner scanner = new Scanner(System.in)) {
+
+            System.out.print("Enter your name: ");
+            String username = scanner.nextLine();
+            System.out.print("Enter your password: ");
+            String pass = scanner.nextLine();
+            System.out.print("Enter your email: ");
+            String email = scanner.nextLine();
+            System.out.print("Enter you cpf: ");
+            String cpf = scanner.nextLine();
+
+
+            String insertSQL = "INSERT INTO users (username, password, email, cpf) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement insertStmt = connection.prepareStatement(insertSQL)) {
+                insertStmt.setString(1, username);
+                insertStmt.setString(2, pass);
+                insertStmt.setString(3, email);
+                insertStmt.setString(4, cpf);
+                insertStmt.executeUpdate();
+                System.out.println("Usuário inserido com sucesso!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
