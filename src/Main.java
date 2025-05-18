5de6fb925cb7d100c3ce1975183f4e0e3d3cbb2f
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-05-18 00:27:37 -0300

    feat: additing connection with mysql through of jdbc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Explicando commit's e PR");
+        String url = "jdbc:mysql://127.0.0.1:3306/users";
+        String user = "root";
+        String password = "1234";
+
     }
 }
